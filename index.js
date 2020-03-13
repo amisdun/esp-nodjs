dd4424de2c073c1296cd99767f5c69294c66a7bd
amisdun <michealdunamis@gmail.com>
2020-03-13 13:18:03 +0000

    added connection socket.io

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const body_parser = require("body-parser")
 const path = require("path")
 
 let app = express()
+const socket_server = require("http").createServer(app)
+
+const io = require("socket.io")(socket_server);
+
 app.use(body_parser.json())
 app.use(body_parser.urlencoded({extended: false}))
 
 // serving static files
-app.use(express.static(__dirname + "/index.html")) //serving static 
+// app.use(express.static(__dirname + "/index.html")) //serving static 
 
 // creating routers
 
@@ -17,16 +21,21 @@ app.get('/', function(req,res,next){
 })
 
 app.post("/api/add_todo", function(req,res,next){
+
+    io.on("connection", () => {
+    
+    })
     let status = req.body.status
 
     if(status) res.json({res: "recieved", data: status})
 })
 
 let port;
+
 if(process.env.NODE_ENV === "production"){port = process.env.PORT}
 else port = 5000
 
 
-app.listen(port, () => {
+socket_server.listen(port, () => {
     console.log(`listening to port ${port}`)
 })
